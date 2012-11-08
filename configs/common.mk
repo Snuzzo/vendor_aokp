ab9c2ac3f5c927c8098a67e43b319bbdc79343a3
Shaun Nuzzo <jrracinfan@gmail.com>
2012-11-08 02:49:19 -0500

    Added Camera to project packages

--- configs/common.mk
+++ configs/common.mk
@@ -5,6 +5,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/common
 include vendor/aokp/configs/themes_common.mk
 
 PRODUCT_PACKAGES += \
+    Camera \
     MusicFX \
     MusicVisualization \
     NoiseField \
